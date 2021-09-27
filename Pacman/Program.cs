008889dbc11a8f99f15e0ed1a91878b89ca3890f
Andreas Nyström <andy.i.flaskan@gmail.com>
2021-09-27 10:54:56 +0200

    Initial Commit

---
+++ Pacman/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace Pacman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var window = new RenderWindow(new VideoMode(828, 900), "Pacman"))
+            {
+                window.Closed += (o, e) => window.Close();
+
+                //Initialize
+                Clock clock = new Clock();
+
+                while (window.IsOpen)
+                {
+                    window.DispatchEvents();
+                    float deltaTime = clock.Restart().AsSeconds();
+
+                    //Update
+
+                    window.Clear(new Color(223, 246, 245));
+                    //Draw
+                    window.Display();
+                }
+            }
+        }
+    }
+}
